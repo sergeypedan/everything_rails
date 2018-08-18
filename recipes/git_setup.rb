b417ce86eb8b23ee67d3b37f5dd5bed99991e029
Sergey Pedan <sergey.pedan@gmail.com>
2018-08-19 02:54:11 +0300

    Disables default githook deployment

--- recipes/git_setup.rb
+++ recipes/git_setup.rb
@@ -22,9 +22,9 @@ bash "Initialize bare Git repo" do
 end
 
 
-template "Puts post-receive hook in place" do
-  action :create
-  mode '0755'
-  path "#{node['deploy']['git_dir']}/hooks/post-receive"
-  source "post_receive_hook.erb"
-end
+# template "Puts post-receive hook in place" do
+#   action :create
+#   mode '0755'
+#   path "#{node['deploy']['git_dir']}/hooks/post-receive"
+#   source "post_receive_hook.erb"
+# end
